664c03e2c6429ecf10ae4d5460bbf58892df6cd1
RichardMiruka <richardonsare15@gmail.com>
2024-03-14 12:37:15 +0300

    a module that provides a function for concatenating two strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
-'''Task 1's module.
-'''
+"""
+This is a module that provides a function for concatenating two strings.
+"""
 
 
 def concat(str1: str, str2: str) -> str:
-    '''Concatenates two strings.
-    '''
+    """
+    This function concatenates two strings and returns the result.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenation of str1 and str2.
+    """
     return str1 + str2
